00245009f1df66e191324f8f6706dd5048e2a4d8
aleksajovanovic <aleksa_jovanovic@hotmail.com>
2018-06-13 18:05:38 -0400

    initial commit

---
+++ wordsworth.js
@@ -0,0 +1,31 @@
+const words = require('an-array-of-english-words');
+var Discord = require('discord.io');
+
+var wordsworth = new Discord.Client({
+    token: "",
+    autorun: true
+});
+
+wordsworth.on('ready', function() {
+    console.log('Logged in as %s - %s\n', wordsworth.username, wordsworth.id);
+});
+
+wordsworth.on('message', function(user, userID, channelID, message, event) {
+    if (message === "ping") {
+        wordsworth.sendMessage({
+            to: channelID,
+            message: "pong"
+        });
+    }
+    if(message.substring(0,1) == '!') {
+        var args = message.substring(1).split(' ');
+
+        while(args.length > 0) {
+            var command = args.shift();
+
+            switch(command) {
+              
+            }
+        }
+    }
+});
