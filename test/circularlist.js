937dac8c835da9263c52fc2b035ee2297dba74cf
Aleksa Jovanovic <aleksa@getstack.ca>
2018-06-17 11:21:12 -0400

    testing for rotation and set

--- test/circularlist.js
+++ test/circularlist.js
@@ -7,7 +7,7 @@ beforeEach(function () {
 })
 
 describe("Base circular list tests", function () {
-    describe("testing initial properties", function () {
+    describe("Testing initial properties", function () {
         it("Testing head", function () {
             assert.equal(circularlist.head, undefined, "Head should be undefined")
         })
@@ -38,5 +38,103 @@ describe("Base circular list tests", function () {
             assert.equal(circularlist.size, 0, "Size should be 0")
             assert.equal(circularlist.iterator, undefined, "Iterator should be undefined")
         })
+        it("Testing multiple push & pop", function () {
+            circularlist.push("junk")
+            assert.equal(circularlist.head, "junk", "Head should be \"junk\"")
+            assert.equal(circularlist.tail, "junk", "Tail should be \"junk\"")
+            assert.equal(circularlist.size, 1, "Size should be 1")
+            assert.equal(circularlist.iterator, "junk", "Iterator should be \"junk\"")
+
+            circularlist.push("hippopotamus")
+            assert.equal(circularlist.head, "junk", "Head should be \"junk\"")
+            assert.equal(circularlist.tail, "hippopotamus", "Tail should be \"hippopotamus\"")
+            assert.equal(circularlist.size, 2, "Size should be 2")
+            assert.equal(circularlist.iterator, "junk", "Iterator should be \"junk\"")
+
+            circularlist.push("tyrannosaurus")
+            assert.equal(circularlist.head, "junk", "Head should be \"junk\"")
+            assert.equal(circularlist.tail, "tyrannosaurus", "Tail should be \"tyrannosaurus\"")
+            assert.equal(circularlist.size, 3, "Size should be 3")
+            assert.equal(circularlist.iterator, "junk", "Iterator should be \"junk\"")
+
+            circularlist.pop()
+            assert.equal(circularlist.head, "junk", "Head should be \"junk\"")
+            assert.equal(circularlist.tail, "hippopotamus", "Tail should be \"hippopotamus\"")
+            assert.equal(circularlist.size, 2, "Size should be 2")
+            assert.equal(circularlist.iterator, "junk", "Iterator should be \"junk\"")
+
+            circularlist.pop()
+            assert.equal(circularlist.head, "junk", "Head should be \"junk\"")
+            assert.equal(circularlist.tail, "junk", "Tail should be \"junk\"")
+            assert.equal(circularlist.size, 1, "Size should be 1")
+            assert.equal(circularlist.iterator, "junk", "Iterator should be \"junk\"")
+
+            circularlist.pop()
+            assert.equal(circularlist.head, undefined, "Head should be undefined")
+            assert.equal(circularlist.tail, undefined, "Tail should be undefined")
+            assert.equal(circularlist.size, 0, "Size should be 0")
+            assert.equal(circularlist.iterator, undefined, "Iterator should be undefined")
+        })
+        
+    })
+
+    describe("Testing rotation", function () {
+        it("Testing rotate", function () {
+            circularlist.push("junk")
+            circularlist.push("hippopotamus")
+            circularlist.push("tyrannosaurus")
+
+            circularlist.rotate()
+            assert.equal(circularlist.head, "hippopotamus", "Head should be \"hippopotamus\"")
+            assert.equal(circularlist.tail, "junk", "Tail should be \"junk\"")
+            assert.equal(circularlist.size, 3, "Size should be 3")
+            assert.equal(circularlist.iterator, "hippopotamus", "Iterator should be \"hippopotamus\"")
+        })
+        it("Testing unrotate", function () {
+            circularlist.push("junk")
+            circularlist.push("hippopotamus")
+            circularlist.push("tyrannosaurus")
+
+            circularlist.unrotate()
+            assert.equal(circularlist.head, "tyrannosaurus", "Head should be \"tyrannosaurus\"")
+            assert.equal(circularlist.tail, "hippopotamus", "Tail should be \"hippopotamus\"")
+            assert.equal(circularlist.size, 3, "Size should be 3")
+            assert.equal(circularlist.iterator, "tyrannosaurus", "Iterator should be \"tyrannosaurus\"")
+        })
+    })
+
+    describe("Testing set", function () {
+        it("Single set", function () {
+            circularlist.push("junk")
+            circularlist.push("hippopotamus")
+            circularlist.push("tyrannosaurus")
+
+            circularlist.set(0, "wharf")
+            assert.equal(circularlist.head, "wharf", "Head should be \"wharf\"")
+            assert.equal(circularlist.tail, "tyrannosaurus", "Tail should be \"tyrannosaurus\"")
+            assert.equal(circularlist.size, 3, "Size should be 3")
+            assert.equal(circularlist.iterator, "wharf", "Iterator should be \"wharf\"")
+        })
+        it("Multiple set", function () {
+            circularlist.push("junk")
+            circularlist.push("hippopotamus")
+            circularlist.push("tyrannosaurus")
+
+            circularlist.set(0, "wharf")
+            circularlist.set(2, "perennial")
+            assert.equal(circularlist.head, "wharf", "Head should be \"wharf\"")
+            assert.equal(circularlist.tail, "perennial", "Tail should be \"perennial\"")
+            assert.equal(circularlist.size, 3, "Size should be 3")
+            assert.equal(circularlist.iterator, "wharf", "Iterator should be \"wharf\"")
+        })
+        //TODO: implement get for circularlist
+        // it("get", function () {
+        //     circularlist.push("junk")
+        //     circularlist.push("hippopotamus")
+        //     circularlist.push("tyrannosaurus")
+
+        //     circularlist.get(0, "wharf")
+           
+        // })
     })
 })
