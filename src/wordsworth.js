1fb06be966365343c6c1fe2f6eee3b1d9c1304fa
aleksajovanovic <aleksa_jovanovic@hotmail.com>
2018-09-09 20:49:41 -0400

    random word generation

--- src/wordsworth.js
+++ src/wordsworth.js
@@ -3,7 +3,8 @@ var Discord = require('discord.io')
 var CircularList = require('../lib/circularlist')
 
 var cipher = new CircularList()
-var participants = []
+var cipherIndexes = []
+
 
 var wordsworth = new Discord.Client({
     token: "NDU1MDA5NDQ1MzgzMjQxNzI4.DgwXeg.GKrKt3IQaiwUuqUF0B-jtvXAny8",
@@ -14,24 +15,26 @@ wordsworth.on('ready', function() {
     console.log('Logged in as %s - %s\n', wordsworth.username, wordsworth.id);
 });
 
-wordsworth.on('message', function(user, userID, channelID, message, event) {
-    if(message.substring(0,3) == 'ww ') {
+wordsworth.on('message', async function(user, userID, channelID, message, event) {
+    if (message.substring(0,3) == 'ww ') {
         var command = message.substring(3)
 
         switch (command) {
             case 'hello':
+                wordsworth.sendMessage({
+                    to: channelID,
+                    message: words.length
+                });
             break
 
             case 'start': 
-                wordsworth.sendMessage({
-                    to: channelID,
-                    message: "starting the cipher"
-                });
+                await countdownCipherStart(channelID)
+                startCipher(channelID)
             break
 
             case 'join':
                 var user = {'user': user, 'userID': userID, 'index': cipher.size()}
-                participants[userID] = cipher.size()
+                cipherIndexes[userID] = cipher.size()
                 cipher.push(user)
             break
 
@@ -39,17 +42,18 @@ wordsworth.on('message', function(user, userID, channelID, message, event) {
                 var user = {'user': user, 'userID': userID, 'index': cipher.size()}
                 var reachedUserToRemove = false
 
-                for(var key in participants) {
+                for (var key in cipherIndexes) {
                     if(reachedUserToRemove) {
-                        --participants[key]
+                        --cipherIndexes[key]
                     }
                     if(key === userID) {
                         reachedUserToRemove = true
                     }
                 }
 
-                var index = participants[userID]
+                var index = cipherIndexes[userID]
                 cipher.remove(index)
+
             case 'end':
                 
             break 
@@ -65,6 +69,60 @@ wordsworth.on('guildMemberAdd', function(member) {
 
 });
 
-function startCipher() {
+function startCipher(channelID) {
+    var cipherWords = generateFiveWords(channelID)
+    console.log(cipherWords.length)
+    for (var i = 0; i < 5; i++) {
+        wordsworth.sendMessage({
+            to: channelID,
+            message: cipherWords[i]
+        });
+    }
+}
 
+function generateFiveWords(channelID) {
+    var fiveWords = []
+    var wordsUsed = []
+
+    for (var i = 0; i < 5; i++) {  
+        var rand
+        do {
+            rand = Math.floor(Math.random() * (words.length))
+        } 
+        while (wordsUsed.includes(rand))
+
+        wordsUsed.push(rand)
+        fiveWords.push(words[rand])
+    }
+
+    return fiveWords
+}
+
+async function countdownCipherStart(channelID) {
+    wordsworth.sendMessage({
+        to: channelID,
+        message: "starting the cipher"
+    });
+    await sleep(3000)
+    wordsworth.sendMessage({
+        to: channelID,
+        message: "3"
+    });
+    await sleep(1000)
+    wordsworth.sendMessage({
+        to: channelID,
+        message: "2"
+    });
+    await sleep(1000)
+    wordsworth.sendMessage({
+        to: channelID,
+        message: "1"
+    });
+    await sleep(500)
+}
+
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
+    })
 }
